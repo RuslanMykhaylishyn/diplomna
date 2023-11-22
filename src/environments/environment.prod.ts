52374f4ff07a2dd7d09eccd3e31da239637caa7d
Ruslan <kingpandoma@gmail.com>
2023-11-22 23:27:10 +0200

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyBIEWUW4vV4IwgcMIuvswjSXhf8vbY4OT4",
+  fbDbUrl: "https://online-shop-b6795-default-rtdb.europe-west1.firebasedatabase.app/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyBIEWUW4vV4IwgcMIuvswjSXhf8vbY4OT4",
+  fbDbUrl: "https://online-shop-b6795-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 /*
